9eff3db5732d23f216612be7b2b0ce91fd986112
Ganesh Barade <ganesh.b@puretechnology.in>
2026-09-04 14:41:33 +0530

    feat: update profile professional summary, adjust About and Experience layout styling, and correct work experience dates

--- src/utils/data/experience.js
+++ src/utils/data/experience.js
@@ -3,14 +3,14 @@ export const experiences = [
     id: 2,
     title: 'Artificial Intelligence Engineer',
     company: 'Pure Technology',
-    duration: 'June 2026 – Present',
+    duration: 'January 2026 – Present',
     logo: '/images/puretech-logo-screenshot.png'
   },
   {
     id: 1,
     title: 'Trainee Data Analytics Engineer',
     company: 'Cravita Technologies',
-    duration: 'Sept 2025 – Feb 2026',
+    duration: 'July 2025 – December 2025',
     logo: '/images/cravita-logo-screenshot.png'
   }
 ]
